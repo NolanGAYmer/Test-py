bf7c6f3d9bcf28dabe2a9b8aa09e25c12d7785ff
NolanGAYmer <nolmouju@gmail.com>
2025-07-16 14:01:57 +0200

    question.py

---
+++ Question.py
@@ -0,0 +1,7 @@
+
+# la creation des variables
+nom = imput=("quel est ton nom ?")
+age = imput=("quel est ton age ?")
+# les reponses des questions
+print=("je suis ") + nom
+print=("j'ai ") + age + (" ans")
